c95748b54e185d99af4a923dbeb45c62b30c12b5
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-11-25 15:24:49 +0900

    Update cal.js

--- cal.js
+++ cal.js
@@ -7,6 +7,11 @@ function update_field(){
     var resultabout =  $('#course').val() * $('#gaku_class').val() * $('#azukari').val() ;
     var result = `${resultabout}円`;
     $('#monoxer_price').text(result);
+    
+    var resultabout_day =  $('#course').val() * $('#gaku_class').val() * $('#azukari').val() / 30 ;
+    var result_day = `約${resultabout_day}円`;
+    $('#monoxer_price_day').text(result_day);
+    
 }
 
 
